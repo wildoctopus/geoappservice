cfad8cc271d034d9aeb0d89e115ec7fd3852b795
pranit07 <pranit.shinde@leasingmonk.com>
2022-05-13 15:29:20 +0200

    routes and Models added

---
+++ application/models/base_model.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from application import db
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+    id = db.Column("id", db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get(cls, id):
+        return cls.query.filter_by(id=id).first()
+
